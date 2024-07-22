d06a6712365eedfcc041c97938f3a3962eb320d1
userabhibhullar <iabhibhullaryt@gmail.com>
2024-07-22 16:28:08 +0530

    connected to mongoose and created todo model

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const PORT = process.env.PORT || 5001;
 
@@ -8,6 +9,17 @@ app.get("/", (req, res) => {
   res.send("this is get api for /");
 });
 
+const connection_string = process.env.CONNECTION_STRING;
+
 app.listen(PORT, () => {
-  console.log(`Server started on port :${PORT}`);
+  console.log(`Server started on port ${PORT}`);
 });
+
+mongoose
+  .connect(connection_string)
+  .then(() => {
+    console.log("Connect to mongoose database.");
+  })
+  .catch((error) => {
+    console.log("Failed to connect to mongoose database.", error.message);
+  });
