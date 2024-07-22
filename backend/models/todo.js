cced2515fa6732cffc4f60296b67d568464ecfdf
userabhibhullar <iabhibhullaryt@gmail.com>
2024-07-22 17:49:59 +0530

    added joi for req data validation for todos, updated author schema

--- backend/models/todo.js
+++ backend/models/todo.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const todoSchema = new mongoose.Schema({
   name: { type: String, required: true, minlength: 3, maxlength: 200 },
-  author: String,
+  author: { type: String, minlength: 3, maxlength: 30 },
   uid: String,
   isComplete: Boolean,
   date: { type: Date, default: new Date() },
--- backend/routes/todos.js
+++ backend/routes/todos.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const { Todo } = require("../models/todo");
+const Joi = require("joi");
 
 const router = express.Router();
 
 router.post("/", async (req, res) => {
+  const schema = Joi.object({
+    name: Joi.string().min(3).max(200).required(),
+    author: Joi.string().min(3).max(30),
+    uid: Joi.string(),
+    isComplete: Joi.boolean(),
+    date: Joi.date(),
+  });
+
+  const { error } = schema.validate(req.body);
+
+  if (error) return res.status(400).send(error.details[0].message);
+
   const { name, author, isComplete, date, uid } = req.body;
 
   let todo = new Todo({
