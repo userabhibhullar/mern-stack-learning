d2e95dab04e24a1ded0ee3a93c00c2b495b8f97b
userabhibhullar <iabhibhullaryt@gmail.com>
2024-07-23 12:46:35 +0530

    created navbar and wrapped the whole app in container from material ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import NavBar from "./components/navBar/NavBar";
+import { Container } from "@mui/material";
 
 const App = () => {
   return (
-    <>
+    <Container maxWidth="md">
       <NavBar />
       <Outlet />
-    </>
+    </Container>
   );
 };
 
--- frontend/src/components/navBar/NavBar.jsx
+++ frontend/src/components/navBar/NavBar.jsx
@@ -1,7 +1,48 @@
+import React from "react";
+import { AppBar, Typography, Toolbar, Button } from "@mui/material";
+import { Link, useNavigate } from "react-router-dom";
+
 const NavBar = () => {
+  const navigate = useNavigate();
+  const linkStyles = {
+    color: "white",
+    textDecoration: "none",
+  };
+  const rootStyles = {
+    flexGrow: 1,
+  };
+
+  const handleSignOut = () => {
+    console.log("navigating");
+    navigate("/signin");
+  };
   return (
     <>
-      <h2>NavBar</h2>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h4" style={rootStyles}>
+            <Link style={linkStyles} to={"/"}>
+              toDoApp;
+            </Link>
+          </Typography>
+          <Typography variant="subtitle2" style={rootStyles}>
+            logged in as Abhi
+          </Typography>
+          <Button onClick={handleSignOut} color="inherit">
+            SignOut
+          </Button>
+          <Button color="inherit">
+            <Link to={"/signin"} style={linkStyles}>
+              Sign In
+            </Link>
+          </Button>
+          <Button color="inherit">
+            <Link to={"/signup"} style={linkStyles}>
+              SignUp
+            </Link>
+          </Button>
+        </Toolbar>
+      </AppBar>
     </>
   );
 };
